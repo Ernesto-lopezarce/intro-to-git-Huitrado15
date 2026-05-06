65bf5bbd29bd23390e049d03959a62b2a9457270
Huitrado15 <luis.huitrado5529@alumnos.udg.mx>
2026-05-06 08:15:32 -0600

    Agrega programa principal

--- Viernes13Meyo/MayorEdad.c
+++ Viernes13Meyo/MayorEdad.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+
+int main(){
+
+    int edad;
+    printf("Indique su edad: ");
+    scanf("%d", &edad);
+    if(edad >= 18){
+        printf("Eres mayor de edad");
+    }else{
+        printf("Mejor vete a dormir");
+    }
+
+    return 0;
+
+}
